6546683cd6dbd87f03f85ba620217f30ddc8a054
ajbt200128 <austinterrio@comcast.net>
2018-05-02 22:44:58 -0400

    changed stupid allow-control-allow-origin thing for the 50th time, now actually pushing it

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -45,7 +45,7 @@ class Application {
         secure("/etc/letsencrypt/live/cutedogs.org/keystore.jks",PASS,"/etc/letsencrypt/live/cutedogs.org/cacerts",PASS);
 
         before( "/*",(request, response) -> {
-            response.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            response.header("Access-Control-Allow-Origin", "*");
             response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
             response.header("Access-Control-Allow-Credentials", "true");
             response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
